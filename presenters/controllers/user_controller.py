d7180c85331a2a1f3c04e1956229990dd4b0d6a1
ymurata <m.yuma0531@gmail.com>
2022-08-10 21:56:39 +0900

    wip

--- presenters/controllers/user_controller.py
+++ presenters/controllers/user_controller.py
@@ -22,26 +22,26 @@ class UsersResponse(BaseModel):
     users: List[User]
 
 
-@router.get("/users", response_model=UsersResponse)
-def get_users(_: Request, usecase: UserUsecase = Depends(UserUsecase)) -> UsersResponse:
+@router.get("/users", response_model=UsersResponse, response_model_exclude={"users": {"__all__": {"id"}}})
+def get_users(usecase: UserUsecase = Depends(UserUsecase)) -> UsersResponse:
     users = usecase.get_users()
     return UsersResponse(users=users)
 
 
-@router.get("/users/{user_id}", response_model=UserResponse)
+@router.get("/users/{user_id}", response_model=UserResponse, response_model_exclude={"user": {"id"}})
 def get_user(_: Request, user_id: int, usecase: UserUsecase = Depends(UserUsecase)) -> UserResponse:
     user = usecase.get_user(id=user_id)
     return UserResponse(user=user)
 
 
 @router.post("/users", response_model=UserResponse)
-def create_user(_: Request, data: CreateUser, usecase: UserUsecase = Depends(UserUsecase)) -> UserResponse:
+def create_user(data: CreateUser, usecase: UserUsecase = Depends(UserUsecase)) -> UserResponse:
     user = usecase.create_user(create_user=data)
     return UserResponse(user=user)
 
 
 @router.put("/users/{user_id}", response_model=UserResponse)
-def update_user(_: Request, user_id: int, data: UpdateUser, usecase: UserUsecase = Depends(UserUsecase)):
+def update_user(user_id: int, data: UpdateUser, usecase: UserUsecase = Depends(UserUsecase)):
     user = usecase.update_user(
         update_user=DUpdateUser(id=user_id, name=data.name))
     return UserResponse(user=user)
